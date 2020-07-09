d7ce2a24d4edb46293fdae81cf6579114be826f7
Mehmet Atakan Çavuşlu <mcavuslu15@ku.edu.tr>
2020-07-09 23:43:07 +0300

    Add networkManager class which is responsible for making API calls from the net.

---
+++ HackerNews-SwiftUI/models/NetworkManager.swift
@@ -0,0 +1,41 @@
+//
+//  NetworkManager.swift
+//  HackerNews-SwiftUI
+//
+//  Created by Atakan Çavuşlu on 9.07.2020.
+//  Copyright © 2020 Atakan Çavuşlu. All rights reserved.
+//
+
+import Foundation
+
+class NetworkManager {
+    
+    func fetchData() {
+        
+        guard let url = URL(string: "http://hn.algolia.com/api/v1/search?tags=front_page") else { return }
+        
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { (data, response, error) in
+            
+            if let e = error {
+                print(e)
+            } else {
+                
+                let decoder = JSONDecoder()
+                guard let safeData = data else { return }
+                
+                do {
+                    let results = try decoder.decode(Results.self, from: safeData)
+                } catch {
+                    print(error)
+                }
+                
+            }
+            
+        }
+        
+        task.resume()
+        
+    }
+    
+}
